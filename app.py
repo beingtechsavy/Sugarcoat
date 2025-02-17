7c278f9410421c14ea2f96044ef6f2700c7f7729
Arpan Padhi <arpanpadhi2004@gmail.com>
2025-02-17 19:29:36 +0530

    n

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    # Run the app without specifying the port.
+    app.run(host='0.0.0.0', debug=False)
